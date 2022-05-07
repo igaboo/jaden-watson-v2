ab62b40728246af2e26caf1d885066b2b04fef52
igaboo <dw.jaden@gmail.com>
2022-05-07 01:26:54 -0700

    added scroll indicator

---
+++ components/ScrollIndicator/ScrollIndicator.js
@@ -0,0 +1,35 @@
+import styles from "./ScrollIndicator.module.scss";
+
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import Link from "next/link";
+
+import { motion } from "framer-motion";
+import Tooltip from "../ToolTip/Tooltip";
+
+function ScrollIndicator({ visible }) {
+  return (
+    <Link href="/#about" scroll={false}>
+      <div className={`${styles.container} ${visible && styles.active}`}>
+        <Tooltip tooltip="Scroll down">
+          <motion.div
+            transition={{
+              type: "spring",
+              duration: 1,
+              repeat: Infinity,
+              repeatType: "mirror",
+            }}
+            animate={{
+              y: ["10%", "-10%"],
+            }}
+          >
+            <FontAwesomeIcon icon={faChevronDown} size="xl" />
+          </motion.div>
+        </Tooltip>
+      </div>
+    </Link>
+  );
+}
+
+export default ScrollIndicator;
