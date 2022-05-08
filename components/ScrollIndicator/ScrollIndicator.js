ac65d5bcb59e65d454cc7cfde2224902a1ba680f
igaboo <dw.jaden@gmail.com>
2022-05-07 22:15:04 -0700

    switched from sass variables to css variables

--- components/ScrollIndicator/ScrollIndicator.js
+++ components/ScrollIndicator/ScrollIndicator.js
@@ -11,23 +11,25 @@ import Tooltip from "../ToolTip/Tooltip";
 function ScrollIndicator({ visible }) {
   return (
     <Link href="/#about" scroll={false}>
-      <div className={`${styles.container} ${visible && styles.active}`}>
-        <Tooltip tooltip="Scroll down">
-          <motion.div
-            transition={{
-              type: "spring",
-              duration: 1,
-              repeat: Infinity,
-              repeatType: "mirror",
-            }}
-            animate={{
-              y: ["10%", "-10%"],
-            }}
-          >
-            <FontAwesomeIcon icon={faChevronDown} size="xl" />
-          </motion.div>
-        </Tooltip>
-      </div>
+      <a>
+        <div className={`${styles.container} ${visible && styles.active}`}>
+          <Tooltip tooltip="Scroll down">
+            <motion.div
+              transition={{
+                type: "spring",
+                duration: 1,
+                repeat: Infinity,
+                repeatType: "mirror",
+              }}
+              animate={{
+                y: ["10%", "-10%"],
+              }}
+            >
+              <FontAwesomeIcon icon={faChevronDown} size="xl" />
+            </motion.div>
+          </Tooltip>
+        </div>
+      </a>
     </Link>
   );
 }
